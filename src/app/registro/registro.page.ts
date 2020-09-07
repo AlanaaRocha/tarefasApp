69b572379d138107514d82dd6bd1f4ed63eaa027
AlanaaRocha <60980150+AlanaaRocha@users.noreply.github.com>
2020-09-07 13:22:28 -0300

    ultimos registros

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators} from '@angular/forms'
 import { Router } from '@angular/router';
 import { CpfValidator } from '../validators/cpf-validator';
 import { ComparaValidator } from '../validators/comparacao-validator';
+import { AlertController } from '@ionic/angular';
+import { UsuariosService } from '../services/usuarios.service';
+import { Usuario } from '../models/Usuario';
 
 @Component({
   selector: 'app-registro',
@@ -49,32 +52,59 @@ public mensagens_validacao = {
   ]
   
 };
-  constructor(private formBuilder: FormBuilder, private router: Router) { 
+  constructor(
+    private formBuilder: FormBuilder, 
+    private router: Router,
+    private usuariosService: UsuariosService,
+    public alertController: AlertController,
+    
+    ) { 
     this.formRegistro = formBuilder.group({
-      nome:['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      cpf:['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(14),
-      CpfValidator.cpfValido
-      ])],
-      data:['', Validators.compose([Validators.required])],
-      genero:['', Validators.compose([Validators.required])],
-      celular:['', Validators.compose([Validators.minLength(10), Validators.maxLength(16)])],
-      email:['', Validators.compose([Validators.required, Validators.email])],
-      senha:['', Validators.compose([Validators.required, Validators.minLength(6)])],
-      confirmar:['', Validators.compose([Validators.required, Validators.minLength(6)])]
+
     }, {
+    
      validator: ComparaValidator('senha', 'confirma')
     });
   }
 
-  ngOnInit() {
-  }
-    public registro(){
-      if (this.formRegistro.valid) {
-        console.log('formulario válido');
-        this.router.navigateByUrl('/home');
-      }else{
-        console.log('formulário inválido')
-      }
+  async ngOnInit() {
+    this.usuariosService.buscarTodos();
+    console.log(this.usuariosService.listaUsuarios);
   }
 
+public async salvarFormulario(){
+  if(this.formRegistro.valid){
+
+let usuario = new Usuario();
+usuario.nome =this.formRegistro.value.nome;
+usuario.cpf = this.formRegistro.value.cpf;
+usuario.dataNascimento = new Date(this.formRegistro.value.dataNascimento);
+usuario.genero = this.formRegistro.value.genero;
+usuario.celular = this.formRegistro.value.celular;
+usuario.email = this.formRegistro.value.email;
+usuario.senha = this.formRegistro.value.senha;
+
+if(await this.usuariosService.salvar(usuario)) {
+  this.exibirAlerta('SUCESSO', 'Usuario salvo com sucesso');
+  this.router.navigateByUrl('/login');
+} else {
+  this.exibirAlerta('ERRO', 'erro ao salvar');
 }
+
+  } else {
+  this.exibirAlerta('ADVERTENCIA', 'Formulario invalido<br/>Verifique os campos do seu formulario');
+  }
+}
+
+   async exibirAlerta(titulo: string, mensagem: string){
+     const alert = await this.alertController.create({
+       header: 'titulo',
+       message: mensagem,
+       buttons: ['OK']
+     });
+
+     await alert.present();
+   }
+  }
+
+
